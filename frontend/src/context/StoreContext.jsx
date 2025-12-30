1639872a8f3d8e40dde2f6a53b6b36a0749a80bb
Hari Venkata Satya Rajesh Desineedi <harivsrdesineedi@gmail.com>
2025-12-30 21:21:50 +0530

    Update API URL to production endpoint

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,6 @@ const StoreContextProvider = (props) => {
 
     const url = "https://food-del-backend-aw57.onrender.com"
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
