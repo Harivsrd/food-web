b2c9ae408c216be0f0b772ccc4fd6004ac81f685
Hari Venkata Satya Rajesh Desineedi <harivsrdesineedi@gmail.com>
2026-08-06 21:41:34 +0530

    Update MongoDB connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://harivsrd:05060708@cluster0.t0lm8b1.mongodb.net/food-del')
+    await mongoose.connect('mongodb+srv://harivsrd:harivsrd@cluster0.t0lm8b1.mongodb.net/?appName=Cluster0')
     .then(()=>{
         console.log("DB connected")
     })
